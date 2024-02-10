9ceb3e3038b91af00d1e68c7c9c7401e8157a12c
Nick <nikoloz.de@gmail.com>
2024-02-10 14:41:28 +0400

    add

---
+++ src/main/java/com/company/rentyourstuff/view/main/MainView.java
@@ -0,0 +1,12 @@
+package com.company.rentyourstuff.view.main;
+
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.app.main.StandardMainView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@Route("")
+@ViewController("nd_MainView")
+@ViewDescriptor("main-view.xml")
+public class MainView extends StandardMainView {
+}
---
+++ src/main/java/com/company/rentyourstuff/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.rentyourstuff.view.user;
+
+import com.company.rentyourstuff.entity.User;
+import com.company.rentyourstuff.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("nd_User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
